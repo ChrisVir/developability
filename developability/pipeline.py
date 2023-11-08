b4e8a8b07206e3a658b3ba92e03e1f8dde40900a
Christopher <crivera@vir.bio>
2023-11-07 17:03:08 -0800

    made cleaning logs optional

--- developability/pipeline.py
+++ developability/pipeline.py
@@ -7,12 +7,13 @@ from developability.descriptors import descriptor_pipeline
 from developability.utils import extract_sequence_from_pdb, clean_logs, determine_chain_type
 
 
-def run_processing_pipeline(input_pdb, output_path=None, nanoshaper_options=None):
+def run_processing_pipeline(input_pdb, output_path=None, nanoshaper_options=None,clean=True):
     """Runs pipeline on individual antibody to extract features. 
     Args:
         input_pdb (path|str): path to input_pdb
         output_path(path|str): path to output
         nanoshaper_options(dict): options for nanoshaper. 
+        clean(bool): whether to clean out logs in execution location.
     Returns: 
         descriptors (pd.DataFrame): one row dataframe. 
     """
@@ -38,6 +39,11 @@ def run_processing_pipeline(input_pdb, output_path=None, nanoshaper_options=None
     descriptors = descriptor_pipeline(light_chain_seq, heavy_chain_seq,residue_pot_file, antibody_name)
     
     # clean out logs in execution location
-    clean_logs()
+    if clean:
+        clean_logs()
     
     return descriptors
+
+
+
+
