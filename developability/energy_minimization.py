b0dde037d8a1809144aca258a23862305b71d98e
Christopher <crivera@vir.bio>
2024-04-14 22:39:53 -0700

    reformat energy_minimization

--- developability/energy_minimization.py
+++ developability/energy_minimization.py
@@ -3,32 +3,32 @@ from biobb_amber.leap.leap_gen_top import leap_gen_top
 from biobb_amber.sander.sander_mdrun import sander_mdrun
 from biobb_amber.process.process_minout import process_minout
 from biobb_amber.ambpdb.amber_to_pdb import amber_to_pdb
-#import matplotlib.pyplot as plt
 import pandas as pd
 
 from pathlib import Path
 
+
 def run_pdb4amber(input_pdb_path, output_pdb_path, properties):
     """ runs pdb4amber to prep pdb to run in amber using biobb_amber
-    Args: 
+    Args:
         input_pdb_path(str|path): path to the input file
         output_pdb_path(str|path|None): path to output file
         properties(dict|None): configuration for running pdb4amber
     Returns: 
         None
     """
-        
-    if not properties: 
+
+    if not properties:
         properties = {'remove_hydrogens': True}
-    
+
     pdb4amber_run(input_pdb_path=input_pdb_path,
-                  output_pdb_path=output_pdb_path, 
-                  properties = properties
+                  output_pdb_path=output_pdb_path,
+                  properties=properties
                   )
 
 
 def run_leap_gen_top(input_pdb_path, output_pdb_path,
-                     output_top_path, output_crd_path, 
+                     output_top_path, output_crd_path,
                      properties=None):
     """ runs amber tools Leap using biobb_amber. The LEAP program does....
     Args: 
@@ -39,17 +39,17 @@ def run_leap_gen_top(input_pdb_path, output_pdb_path,
     Returns: 
         None
     """
-    if not properties: 
-        properties = { "forcefield":["protein.ff19SB"]}
+    if not properties:
+        properties = {"forcefield": ["protein.ff19SB"]}
 
     leap_gen_top(input_pdb_path=str(input_pdb_path),
-           output_pdb_path=output_pdb_path,
-           output_top_path=output_top_path,
-           output_crd_path=output_crd_path,
-           properties=properties)
+                 output_pdb_path=output_pdb_path,
+                 output_top_path=output_top_path,
+                 output_crd_path=output_crd_path,
+                 properties=properties)
 
 
-def extract_energy_path(sander_log, output_path): 
+def extract_energy_path(sander_log, output_path):
     """ Uses biobb_amber minout to extract energy from sander output. 
     Args: 
         sander_log(str): Path to sander log file. 
@@ -57,24 +57,24 @@ def extract_energy_path(sander_log, output_path):
         None
     """
     process_minout(input_log_path=sander_log,
-            output_dat_path=output_path,
-            properties={"terms" : ['ENERGY']}
-    )
+                   output_dat_path=output_path,
+                   properties={"terms": ['ENERGY']}
+                   )
 
 
-def plot_energy_path(dat_path, max_enery = 1000, figsize=(4,3)): 
+def plot_energy_path(dat_path, max_enery=1000, figsize=(4, 3)):
     """"plots the energy path using matplotlib"""
     file = Path(dat_path)
-    df = pd.read_csv(file, delim_whitespace= True, 
-                     header = None, 
-                     names = ['Iteration', 'Energy']
+    df = pd.read_csv(file, delim_whitespace=True,
+                     header=None,
+                     names=['Iteration', 'Energy']
                      )
-    df=df.query('Energy<=@max_energy')
-    ax = df.plot(x = "Iteration", figsize = (4, 3))
-    #TODO add savepath
+    df = df.query('Energy<=@max_energy')
+    ax = df.plot(x="Iteration", figsize=(4, 3))
+    # TODO add savepath
 
 
-def run_sander(input_top, input_crd, input_ref, output_traj, output_rst, output_log, 
+def run_sander(input_top, input_crd, input_ref, output_traj, output_rst, output_log,
                properties=None):
     """Executes the Sander molecular dynamics algorithm via biobb_amber for energy minmization. 
     Args: 
@@ -88,18 +88,18 @@ def run_sander(input_top, input_crd, input_ref, output_traj, output_rst, output_
     Returns: 
         None
     """
-    if not properties: 
+    if not properties:
         properties = {
-            'simulation_type' : "min_vacuo",
-            "mdin" : { 
-                'maxcyc' : 500,
-                'ntpr' : 500,
-                'ntr' : 1,
-                'restraintmask' : '\":*&!@H=\"',
-                'restraint_wt' : 50.0
-                }
-                }
-    
+            'simulation_type': "min_vacuo",
+            "mdin": {
+                'maxcyc': 500,
+                'ntpr': 500,
+                'ntr': 1,
+                'restraintmask': '\":*&!@H=\"',
+                'restraint_wt': 50.0
+            }
+        }
+
     sander_mdrun(input_top_path=input_top,
                  input_crd_path=input_crd,
                  input_ref_path=input_ref,
@@ -107,8 +107,9 @@ def run_sander(input_top, input_crd, input_ref, output_traj, output_rst, output_
                  output_rst_path=output_rst,
                  output_log_path=output_log,
                  properties=properties)
-    
-def convert_amber_to_pdb(input_top, input_crd, output_pdb): 
+
+
+def convert_amber_to_pdb(input_top, input_crd, output_pdb):
     """"Uses biobb_amber amber to pdb to convert topology and coordinate file to pdb
     Args:
         input_top(str): input topology file
@@ -117,20 +118,20 @@ def convert_amber_to_pdb(input_top, input_crd, output_pdb):
     Returns:
         None
     """
-    return amber_to_pdb(input_top_path = input_top, 
-                        input_crd_path = input_crd, 
-                        output_pdb_path = output_pdb
+    return amber_to_pdb(input_top_path=input_top,
+                        input_crd_path=input_crd,
+                        output_pdb_path=output_pdb
                         )
-    
 
-class EnergyMinimizer(object): 
-    def __init__(self, 
-                 input_pdb, 
-                 output_pdb=None, 
-                 output_path=None, 
-                 amber_properties=None, 
+
+class EnergyMinimizer(object):
+    def __init__(self,
+                 input_pdb,
+                 output_pdb=None,
+                 output_path=None,
+                 amber_properties=None,
                  leap_properties=None,
-                 sander_properties=None, 
+                 sander_properties=None,
                  ):
         """ Class for runninng energy minimization. Given a PDB, the class first 
         uses the pdb4amber program to prepare the pdb, then runs leap and finally relaxes
@@ -146,52 +147,50 @@ class EnergyMinimizer(object):
 
         self.input_pdb = input_pdb
         self.name = Path(input_pdb).name.split('.')[0]
-        
 
         if not output_path:
-            output_path = Path(self.input_pdb).parent /f'{self.name}_outputs'
-            
-        if not output_path.exists(): 
+            output_path = Path(self.input_pdb).parent / f'{self.name}_outputs'
+
+        if not output_path.exists():
             output_path.mkdir()
-        self.output_path = output_path 
-        
-        # output_pdb is the final output. 
+        self.output_path = output_path
+
+        # output_pdb is the final output.
         if not output_pdb:
-            output_pdb =  self.output_path / f'{self.name}.energy_minimized.pdb'
+            output_pdb = self.output_path / f'{self.name}.energy_minimized.pdb'
         self.output_pdb = output_pdb
-        
-        #pdb4amber settings
-        if not amber_properties: 
+
+        # pdb4amber settings
+        if not amber_properties:
             amber_properties = {'remove_hydrogens': True}
         self.amber_properties = amber_properties
         self.pdb4amber_pdb = self.output_path/'pdb4amber.pdb'
 
         # leap settings
-        if not leap_properties: 
-            leap_properties = { "forcefield":["protein.ff19SB"]}
+        if not leap_properties:
+            leap_properties = {"forcefield": ["protein.ff19SB"]}
         self.leap_properties = leap_properties
         self.leap_pdb = str(self.output_path/'leap.pdb')
-        self.leap_top = str(self.output_path/'leap.top')   #topology
-        self.leap_crd = str(self.output_path/'leap.crd')   #coordniate
+        self.leap_top = str(self.output_path/'leap.top')  # topology
+        self.leap_crd = str(self.output_path/'leap.crd')  # coordniate
 
         # md_sander settings TODO: consult with Kevin
-        if not sander_properties: 
+        if not sander_properties:
             sander_properties = {
-                'simulation_type' : "min_vacuo",
-                "mdin" : { 
-                    'maxcyc' : 500,
-                    'ntpr' : 5,
-                    'ntr' : 1,
-                    'restraintmask' : '\":*&!@H=\"',
-                    'restraint_wt' : 50.0
-                    }
-                    }
+                'simulation_type': "min_vacuo",
+                "mdin": {
+                    'maxcyc': 500,
+                    'ntpr': 5,
+                    'ntr': 1,
+                    'restraintmask': '\":*&!@H=\"',
+                    'restraint_wt': 50.0
+                }
+            }
 
         self.sander_properties = sander_properties
         self.output_traj = str(self.output_path/'sander.x')
         self.output_rst = str(self.output_path/'sander.rst')
         self.sander_log = str(self.output_path/'sander.log')
-
 
     def minimize_energy(self, verbose=True):
         """Executes all the steps for the energy minimization. 
@@ -204,59 +203,59 @@ class EnergyMinimizer(object):
             verbose(bool): if True, print out logs
         """
 
-        if verbose: 
+        if verbose:
             print(f'Starting pdb4amber with {self.input_pdb}. \n')
 
         run_pdb4amber(input_pdb_path=self.input_pdb,
                       output_pdb_path=self.pdb4amber_pdb,
-                      properties = self.amber_properties
-             );
-        
-        if verbose: 
+                      properties=self.amber_properties
+                      )
+
+        if verbose:
             print('Finished pdb4amber. \n')
             print(f'Starting Leap with {self.pdb4amber_pdb}. \n')
 
         run_leap_gen_top(input_pdb_path=self.pdb4amber_pdb,
-           output_pdb_path=self.leap_pdb,
-           output_top_path=self.leap_top,
-           output_crd_path=self.leap_crd,
-           properties=self.leap_properties
-           )
-        
-        if verbose: 
+                         output_pdb_path=self.leap_pdb,
+                         output_top_path=self.leap_top,
+                         output_crd_path=self.leap_crd,
+                         properties=self.leap_properties
+                         )
+
+        if verbose:
             print('Finished Leap. \n')
             print(f'Starting Sander with {self.leap_pdb}. \n')
-        
-        run_sander(input_top = self.leap_top, 
-                   input_crd = self.leap_crd,
-                   input_ref =  self.leap_crd, 
-                   output_traj = self.output_traj, 
-                   output_rst = self.output_rst,  
-                   output_log = self.sander_log,
-                   properties = self.sander_properties
+
+        run_sander(input_top=self.leap_top,
+                   input_crd=self.leap_crd,
+                   input_ref=self.leap_crd,
+                   output_traj=self.output_traj,
+                   output_rst=self.output_rst,
+                   output_log=self.sander_log,
+                   properties=self.sander_properties
                    )
-        
-        if verbose: 
+
+        if verbose:
             print('Finished Sander. \n')
             print('Converting Sander results to pdb. \n')
-        
-        convert_amber_to_pdb(self.leap_top, self.output_rst, self.output_pdb) 
 
-        if verbose: 
+        convert_amber_to_pdb(self.leap_top, self.output_rst, self.output_pdb)
+
+        if verbose:
             print('Finished conversion \n')
 
         return self.output_pdb
-            
-        
-if __name__ =='__main__':
 
-    input_pdb=Path().cwd().parent /'Notebooks/abagovomab_forChris.pdb'
+
+if __name__ == '__main__':
+
+    input_pdb = Path().cwd().parent / 'Notebooks/abagovomab_forChris.pdb'
     minimizer = EnergyMinimizer(input_pdb)
 
     minimizer.minimize_energy()
-    
 
-# TODO Write tests 
+
+# TODO Write tests
 # TODO: add logger maybe
-# TODO: add code to extract the graphs. 
-# TODO: clean up logs. (consider actually adding more paths. ) 
+# TODO: add code to extract the graphs.
+# TODO: clean up logs. (consider actually adding more paths. )
