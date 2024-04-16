6efc61d8ec0d3061d1956e4697f332c88e57a342
Christopher <crivera@vir.bio>
2024-04-16 14:50:57 -0700

    formatted code

--- developability/antibody_structure.py
+++ developability/antibody_structure.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import nglview as ngl
-#from ImmuneBuilder import  ABodyBuilder2
-from pathlib  import Path
-import matplotlib.pyplot as plt
+# from ImmuneBuilder import  ABodyBuilder2
+from pathlib import Path
 from Bio.PDB import PDBParser
 from Bio.PDB.PDBIO import PDBIO
 from tqdm.notebook import tqdm
@@ -10,68 +8,62 @@ from tqdm.notebook import tqdm
 
 def renumber_pdb(input_pdb, output_pdb=None):
     """Renumbers residues for pdb file
-    Args: 
+    Args:
         input_pdb(str|Path): path to input pdb
         output_pdb(str|Path): path to pdb for output
-    Returns: 
+    Returns:
         None
     """
 
     # parse the pdb and update numbers for each chain
     parser = PDBParser()
-    struct= parser.get_structure('pdb', str(input_pdb))
-    for model in struct: 
+    struct = parser.get_structure('pdb', str(input_pdb))
+    for model in struct:
         for chain in model:
             num = 1
             for residue in chain:
                 residue.id = (' ', num, ' ')
-                num+=1
+                num += 1
 
     # save the pdb
-    if not output_pdb: 
+    if not output_pdb:
         output_pdb = Path(input_pdb).with_suffix('.renumbered.pdb')
     pdb_io = PDBIO()
     pdb_io.set_structure(struct)
     pdb_io.save(str(output_pdb))
-    
-  
+
+
 def predict_antibody_structures(sequences, output_dir=None):
-    """Given a dataframe with sequences, use ABodyBuilder2 model to predict sequences
-    Args: 
-        sequence(pd.DataFrame|str|Path): data frame with columns 'Name', 'VH', 'VL'
-        output_dir(path|Str): location to save models. 
-    Returns: 
+    """Given a dataframe with sequences, use ABodyBuilder2 model to predict 
+       sequences.
+    Args:
+        sequence(pd.DataFrame|str|Path): data frame with columns 'Name', 'VH',
+                                        'VL'
+        output_dir(path|Str): location to save models.
+    Returns:
         None
     """
 
     predictor = ABodyBuilder2()
     if isinstance(sequences, str) or isinstance(sequences, Path):
         sequences = pd.read_csv(sequences)
-    elif isinstance(sequences, pd.DataFrame): 
+    elif isinstance(sequences, pd.DataFrame):
         pass
     else:
         print('Not antibodies')
 
-    sequences= sequences.dropna()
-    errors=[]
+    sequences = sequences.dropna()
+    errors = []
 
     iterator = zip(sequences['Name'], sequences['VH'], sequences['VL'])
     n = len(sequences)
     for name, hc, lc in tqdm(iterator, total=n):
-        sequences = {'H': hc,'L': lc}
+        sequences = {'H': hc, 'L': lc}
         try:
             antibody = predictor.predict(sequences)
 
-            if output_dir: 
+            if output_dir:
                 output_file = output_dir/f'{name}.pdb'
                 antibody.save(str(output_file))
         except AssertionError:
             errors.append(name)
-            
-        
-        
-    
-
-
-
-
--- developability/pipeline.py
+++ developability/pipeline.py
@@ -4,46 +4,47 @@ from developability.energy_minimization import EnergyMinimizer
 from developability.electrostatics import APBS
 from developability.surface import SurfacePotential
 from developability.descriptors import descriptor_pipeline
-from developability.utils import extract_sequence_from_pdb, clean_logs, determine_chain_type
+from developability.utils import (extract_sequence_from_pdb, clean_logs,
+                                  determine_chain_type
+                                  )
 
 
-def run_processing_pipeline(input_pdb, output_path=None, nanoshaper_options=None,clean=True):
-    """Runs pipeline on individual antibody to extract features. 
+def run_processing_pipeline(input_pdb, output_path=None,
+                            nanoshaper_options=None, clean=True):
+    """Runs pipeline on individual antibody to extract features.
     Args:
         input_pdb (path|str): path to input_pdb
         output_path(path|str): path to output
-        nanoshaper_options(dict): options for nanoshaper. 
+        nanoshaper_options(dict): options for nanoshaper.
         clean(bool): whether to clean out logs in execution location.
-    Returns: 
-        descriptors (pd.DataFrame): one row dataframe. 
+    Returns:
+        descriptors (pd.DataFrame): one row dataframe.
     """
-    
-    # minimize the energy 
-    minimizer=EnergyMinimizer(input_pdb, output_path = output_path)
+
+    # minimize the energy
+    minimizer = EnergyMinimizer(input_pdb, output_path=output_path)
     minimized_pdb = minimizer.minimize_energy()
 
     # compute electrostatics
-    apbs = APBS(minimized_pdb )
+    apbs = APBS(minimized_pdb)
     pqr_file, dx_file = apbs.calculate_potential()
 
     # compute the surface
-    surface = SurfacePotential(pqr_file, dx_file, nanoshaper_options=nanoshaper_options)
+    surface = SurfacePotential(
+        pqr_file, dx_file, nanoshaper_options=nanoshaper_options)
     residue_pot_file = surface.calculate_surface_potential()
 
     # get the sequence from the input pdb
     sequences = extract_sequence_from_pdb(input_pdb)
-    light_chain_seq , heavy_chain_seq = determine_chain_type(sequences)
+    light_chain_seq, heavy_chain_seq = determine_chain_type(sequences)
 
     # Calculate the descriptors
     antibody_name = input_pdb.name.split('.')[0]
-    descriptors = descriptor_pipeline(light_chain_seq, heavy_chain_seq,residue_pot_file, antibody_name)
-    
+    descriptors = descriptor_pipeline(
+        light_chain_seq, heavy_chain_seq, residue_pot_file, antibody_name)
+
     # clean out logs in execution location
     if clean:
         clean_logs()
-    
+
     return descriptors
-
-
-
-
