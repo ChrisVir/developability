705ad293171dee049ac428531b5cd82e74b4ca02
ChrisVir <crivera@vir.bio>
2024-04-30 18:42:29 -0700

    update dockerfile and notebooks.

--- Dockerfiles/resources/dev_models/entrypoints.py
+++ Dockerfiles/resources/dev_models/entrypoints.py
@@ -3,41 +3,69 @@ import click
 import pandas as pd
 from pathlib import Path
 from datetime import datetime
-from mlflow import pyfunc
+from mlflow import sklearn
+from mlflow.models import get_model_info
 
 
 base_path = Path('/app/dev_models/')
 model_paths = {'heparin_regression': base_path/'hep_regressor',
-               'heparin_classification': base_path/'heparin_classifier'
+               'heparin_classification': base_path/'hep_classifier'
                }
 
 
-def load_model(model_name):
-    model_path = model_paths[model_name]
-    return pyfunc.load_model(model_path)
+def extract_metadata_from_signature(model_path):
+    """extracts the model target names from a model signature"""
+
+    def get_outputs(signature):
+        return [output['name'] for output in signature.outputs.to_dict()]
+
+    def get_inputs(signature):
+        return [input['name'] for input in signature.inputs.to_dict()]
+
+    signature = get_model_info(str(model_path)).signature
+
+    return get_inputs(signature), get_outputs(signature)
 
 
-def predict(model, X):
-    """ predict with a model"""
-    predictions = pd.DataFrame(model.predict(X), index=X.index)
-    return predictions
+def load_model(model_path):
+    return sklearn.load_model(str(model_path))
 
 
-def load_input_data(filepath):
+def load_input_data(filepath, columns=None):
     filepath = Path(filepath)
     if filepath.suffix in ['.pq', '.parquet']:
-        return pd.read_parquet(filepath)
+        return pd.read_parquet(filepath, columns=columns)
 
     elif filepath.suffix == '.csv':
-        return pd.read_csv(filepath)
+        return pd.read_csv(filepath, columns=columns)
 
 
 def save_predictions(predictions, filename):
+    """Save predictions in dataframe to file"""
     if filename.suffix in ['.pq', '.parquet']:
         predictions.to_parquet(filename)
 
     if filename.suffix == '.csv':
         predictions.to_csv(filename)
+
+
+def batch_predict(model_path, input_data_path, predict_proba=False):
+    """ Loads model and predicts"""
+
+    model = load_model(model_path)
+    inputs, targets = extract_metadata_from_signature(model_path)
+
+    X = load_input_data(input_data_path, columns=inputs)
+
+    if predict_proba:
+        preds = model.predict_proba(X)[:, 1]
+    else:
+        preds = model.predict(X)
+
+    predictions = pd.DataFrame(preds, index=X.index,
+                               columns=targets)
+    predictions.index.name = 'antibody'
+    return predictions
 
 
 @click.command()
@@ -48,14 +76,11 @@ def predict_heparin_binding(input_data_path, output_filename=None,
                             output_dir=None):
     """ predict model """
 
-    input_data_path = Path(input_data_path)
-    X = load_input_data(input_data_path)
-
-    model = load_model('heparin_regression')
-    predictions = predict(model, X)
+    model_path = model_paths['heparin_regression']
+    predictions = batch_predict(model_path, input_data_path)
 
     if not output_dir:
-        output_dir = input_data_path.parent
+        output_dir = Path(input_data_path).parent
 
     if not output_filename:
         today = datetime.today().strftime('%m-%d-%Y')
@@ -72,14 +97,11 @@ def classify_heparin_binding(input_data_path, output_filename=None,
                              output_dir=None):
     """ classify heparin_binding """
 
-    input_data_path = Path(input_data_path)
-    X = load_input_data(input_data_path)
-
-    model = load_model('heparin_classification')
-    predictions = predict(model, X)
+    model_path = model_paths['heparin_classification']
+    predictions = batch_predict(model_path, input_data_path, True)
 
     if not output_dir:
-        output_dir = input_data_path.parent
+        output_dir = Path(input_data_path).parent
 
     if not output_filename:
         today = datetime.today().strftime('%m-%d-%Y')
